518d511a86e97cd385f2f3564d3d42e8412d13ac
drawholka <mamrokhavlad@gmail.com>
2025-02-07 17:17:50 +0200

    methods implemented

--- src/Terminal.cpp
+++ src/Terminal.cpp
@@ -1,1 +1,17 @@
 #include "Terminal.hpp"
+
+
+Terminal::Terminal(value_type val)
+{
+	value = val;
+};
+
+value_type Terminal::interpret()
+{
+	return value;
+};
+
+void Terminal::print()
+{
+	std::cout << value << std::endl;
+};
