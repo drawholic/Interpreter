4eac12a82de6c74fb03b5490833a4031d6dd5f43
drawholka <mamrokhavlad@gmail.com>
2025-02-07 15:57:01 +0200

    Nonterminal class declaration

---
+++ include/NonTerminal.hpp
@@ -0,0 +1,32 @@
+#ifndef NON_TERMINAL_HPP
+#define NON_TERMINAL_HPP
+
+#include "AbstractExpression.hpp"
+
+enum Operator{
+	ADD,
+	SUB,
+
+	MUL,
+	DIV,
+
+	POW,
+
+	LP,
+	RP,
+}
+
+class NonTerminal: public AbstractExpression
+{
+	Operator op;
+	AbstractExpression* left;
+	AbstractExpression* right;
+
+public:
+	NonTerminal();
+	value_type interpret() override;
+	void print() override;
+
+}
+
+#endif
