c8e9959bc451c4fe205308861846b7061c60d4f6
drawholka <mamrokhavlad@gmail.com>
2025-02-07 15:54:40 +0200

    AbstractExpression list include and typedef of list of AbstractExpression pointers

--- include/AbstractExpression.hpp
+++ include/AbstractExpression.hpp
@@ -1,15 +1,22 @@
 #ifndef ABSTRACT_EXPRESSION_HPP
 #define ABSTRACT_EXPRESSION_HPP
 
+#include <list>
+
+typedef value_type int;
+
 
 class AbstractExpression
 {
 
 
 public:
-	virtual int interpret();
+	virtual value_type interpret();
 	virtual void print();
 };
+
+
+typedef std::list<AbstractExpression*> token_list
 
 
 #endif
