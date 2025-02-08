21ca4d05d94f47e9fb601eb3b1fa4363cfa0dd6c
drawholka <mamrokhavlad@gmail.com>
2025-02-08 09:19:13 +0200

    parentheses parse, parentheses check

--- src/Parser.cpp
+++ src/Parser.cpp
@@ -71,10 +71,59 @@ void Parser::parse_mul_div(token_list& tokens)
 	}
 };
 
-void Parser::parse_parentheses(token_list&)
+void Parser::parse_parentheses(token_list& tokens)
 {
+	exp_iter lp = tokens.begin();
+	exp_iter rp;
+	exp_iter pend = tokens.end();
 
+	unsigned counter = 1;
+	while(lp != pend)
+	{
+		lp = std::find_if(it, pend, is_left_parenthesis);
+
+		if(it == pend)
+			return;
+
+
+
+	};
 };
+
+bool Parser::is_left_parenthesis(AbstractExpression* exp){
+	bool res = false;
+
+	if(!exp->is_term())
+	{
+		NonTerminal* non_term = dynamic_cast<NonTerminal*>(exp);
+		Operator op = non_term->get_op();
+		res =  non_term->empty() && op == LP;
+	};
+	return res;
+};
+bool Parser::is_right_parenthesis(AbstractExpression* exp){
+	bool res = false;
+
+	if(!exp->is_term())
+	{
+		NonTerminal* non_term = dynamic_cast<NonTerminal*>(exp);
+		Operator op = non_term->get_op();
+		res =  non_term->empty() && op == RP;
+	};
+	return res;
+};
+bool Parser::is_parenthesis(AbstractExpression* exp){
+	bool res = false;
+
+	if(!exp->is_term())
+	{
+		NonTerminal* non_term = dynamic_cast<NonTerminal*>(exp);
+		Operator op = non_term->get_op();
+		res =  non_term->empty() && (op == LP || op == RP);
+	};
+	return res;
+};
+
 
 bool Parser::is_add_sub(AbstractExpression* exp)
 {
