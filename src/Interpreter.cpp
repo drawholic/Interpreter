093b1d24e000ecba696c1896c7271c3fe8b141a4
drawholka <mamrokhavlad@gmail.com>
2025-02-08 09:05:29 +0200

    constructor and print_tokens definition

--- src/Interpreter.cpp
+++ src/Interpreter.cpp
@@ -7,4 +7,24 @@ Interpreter::Interpreter(){
 	std::cout << "Enter arithmetic expression: ";
 	std::getline(std::cin, input_buf);
 	std::cout << "Entered: " << input_buf << std::endl;
+
+	sc.clear_whitespaces(input_buf);
+
+	std::cout << "After clear: " << input_buf << std::endl;
+
+	sc.gen_expressions_list(input_buf, tokens);
+
+	print_tokens();
+
+	ps.build_tree(tokens);	
+
+	std::cout << "After build tree: ";
+	print_tokens();
+};
+
+void Interpreter::print_tokens()
+{
+	for(auto i : tokens)
+		i->print();
+
 };
