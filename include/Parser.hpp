c49ef7c6e0f447193576dfaf1b20609e764b5a09
drawholka <mamrokhavlad@gmail.com>
2025-02-08 15:20:39 +0200

    power and unary parse methods declare. respective static is methods added

--- include/Parser.hpp
+++ include/Parser.hpp
@@ -14,15 +14,18 @@ class Parser
 	static bool is_left_parenthesis(AbstractExpression*);
 	static bool is_right_parenthesis(AbstractExpression*);
 	static bool is_parenthesis(AbstractExpression*);
-
-
+	static bool is_power(AbstractExpression*);
+	static bool is_unary(AbstractExpression*);
 
 public:
 	Parser();
 	void build_tree(token_list&);
+	void parse_unary(token_list&);
 	void parse_add_sub(token_list&);
 	void parse_mul_div(token_list&);
 	void parse_parentheses(token_list&);
+	void parse_power(token_list&);
+
 };
 
 #endif
