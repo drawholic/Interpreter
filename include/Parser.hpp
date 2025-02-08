d7781880df4458f3905f1b0808dc973aea25e95d
drawholka <mamrokhavlad@gmail.com>
2025-02-08 08:58:43 +0200

    declaration methods of checking the operator and parse functions. includes

--- include/Parser.hpp
+++ include/Parser.hpp
@@ -2,17 +2,22 @@
 #define PARSER_HPP
 
 #include "AbstractExpression.hpp"
+#include "NonTerminal.hpp"
+#include <algorithm>
 
 class Parser
 {
 	AbstractExpression* tree;
 
+	static bool is_add_sub(AbstractExpression*);
+	static bool is_mul_div(AbstractExpression*);
+
 public:
 	Parser();
-	void buildTree(AbstractExpression*);
-	void parse_add_sub();
-	void parse_mul_div();
-	void parser_parentheses();
+	void build_tree(token_list&);
+	void parse_add_sub(token_list&);
+	void parse_mul_div(token_list&);
+	void parse_parentheses(token_list&);
 };
 
 #endif
