360ce40d66f6d2de0c366a71f95122b0ff575d26
drawholka <mamrokhavlad@gmail.com>
2025-02-08 09:01:03 +0200

    virtual is_term declaration. typedef for iterator to AbstractExpression list

--- include/AbstractExpression.hpp
+++ include/AbstractExpression.hpp
@@ -8,16 +8,15 @@ typedef int value_type;
 
 class AbstractExpression
 {
-
-
 public:
 	virtual ~AbstractExpression();
 	virtual value_type interpret() = 0;
 	virtual void print() = 0;
+	virtual bool is_term() = 0;
 };
 
 
 typedef std::list<AbstractExpression*> token_list;
-
+typedef std::list<AbstractExpression*>::iterator exp_iter;
 
 #endif
